bf9146abe469d326c9d7286bcf3cd81614d3892f
Zikebuz <71839001+Zikebuz@users.noreply.github.com>
2023-03-27 01:27:08 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
+const REACT_APP_API_URL = 'http://www.omdbapi.com?apikey=529dc4a3'
+
 const App = () => {
 
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+        const response = await fetch(`${REACT_APP_API_URL}&s=${title}`);
         const data = await response.json()
 
         setMovies(data.Search);
