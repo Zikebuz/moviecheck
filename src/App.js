2d1c06bec5644cbfbb30ef40d4c6b7c278dbd5e7
Zikebuz <71839001+Zikebuz@users.noreply.github.com>
2023-03-27 01:54:23 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>Movie Hunt</h1>
+            <h1>Movie Check</h1>
 
             <div className="search">
                 <input
